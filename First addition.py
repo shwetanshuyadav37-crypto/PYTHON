fc27ea4e38207cd631265c9825b6cd963607609e
Shwetanshu Yadav <shwetanshuyadav37@gmail.com>
2026-08-13 03:44:44 +0530

    Add files via upload

---
+++ First addition.py
@@ -0,0 +1,10 @@
+#take input from user and store them in a variable
+fnum =input('Enter first number')
+snum =input('Enter second number')
+
+#print(type(fnum) ,type(snum))
+#add the two variable
+result = int(fnum) + int(snum)
+
+# print the result
+print(result)
---
+++ anthony1.py
@@ -0,0 +1,119 @@
+# python is case sensitive language
+print("Hello World",1,4.5,True,sep="/")
+print("hello",end="-")
+print("world")
+
+#// DATA TYPES:-
+
+#boolean data type
+
+print(True)
+print(False)
+
+#integer data type
+print(8)
+print(1e308)
+
+#Decimal/float data type
+print(8.5)
+print(1.7e308)
+
+#Text/String
+print("hello babu")
+
+#complex
+print(5+6j)
+
+#List
+print([1,2,3,4,5])
+
+#Tuple
+print((1,2,3,4,5))
+
+#Sets
+print({1,2,3,4,5})
+
+#Dicitionary
+print({"name":"Nitish","gender":"Male","weight":70})
+
+#type(it is a function)
+type([1,2,3])
+
+#VARIABLES:-
+
+#1:- Dyanamic typing:-
+# Where we do not type the data type...it automatically get that data type
+# Python support dyanamic typing
+
+#2:- Static typing:-
+ # in this whenever we declare a variable we first describe data type
+ # Like: int a=5
+ 
+ #3:-Dyanamic Binding:-
+ # a=5
+ #print(a)
+ #a=("nitish")
+ #print(a)
+ 
+ #4:- Static Binding:-
+ #int a=5   (only one variable is used)
+ 
+ # Multi line comments are not allowed in python
+ # Python interpret line by line
+ 
+ 
+ # Keywords & Identifiers :-
+ 
+ # Keywords are some reserved words that a interpreter used to interpret high level code to machine code
+ # 32 keywords are used in keywords
+ 
+ # Identifiers:-whenever we create or give a name like variables , creating your own function , etc.
+ # Creating a name in python are identifiers  
+ 
+ # You can't start a variable with a digit.
+ # You can use a special char-> "_".(to declare a variable)
+ # Other keywords are not allowed like %,/,etc.
+ # Keywords are not identifiers.
+ 
+ # Literals:-Literals are the data which is givem to a variable or constant. It is also called as a fixed value
+ 
+ # Binary Literals:
+a = 0b1010
+print(a)
+
+# Deciaml LIterals:
+b=100
+print(b)
+
+# Octal Literals:
+c=0o310
+print(c)
+
+# Hexadeciaml Literals:
+d=0x12c
+print(d)
+
+print(1.5e2)
+
+#complex literals:
+x=3.14j
+print(x.real)
+print(x.imag)
+
+#Boolean literals:
+a = True + 4
+b = False + 10
+print("a:",a)
+print("b:",b)
+
+#None literals:
+a = None
+print(a)
+
+#string literals:
+s1 = 'shwetanshu'
+print(s1)
+ 
+ 
+    
+ 
